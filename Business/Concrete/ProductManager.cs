68540de578ca3d5afe5a20bccbb57bf7b2490b01
Yasin <yasincaykur@gmail.com>
2023-10-05 12:22:38 +0300

    business rules added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,8 +2,8 @@ using System.ComponentModel.DataAnnotations;
 using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
-using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -16,10 +16,12 @@ namespace Business.Concrete;
 public class ProductManager : IProductService
 {
     private readonly IProductDal _productDal;
+    private readonly ICategoryService _categoryService;
 
-    public ProductManager(IProductDal productDal)
+    public ProductManager(IProductDal productDal, ICategoryService categoryService)
     {
         _productDal = productDal;
+        _categoryService = categoryService;
     }
 
     public IDataResult<List<Product>> GetAll()
@@ -31,18 +33,28 @@ public class ProductManager : IProductService
     [ValidationAspect(typeof(ProductValidator))]
     public IResult Add(Product product)
     {
+        var result = BusinessRules.Run(CheckProductNameIsUnique(product.ProductName),
+            CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+            CheckIfCategoryLimitExceeded());
+        if (result != null)
+        {
+            return result;
+        }
+
         _productDal.Add(product);
         return new SuccessResult(Messages.ProductAdded);
     }
 
     public IDataResult<List<Product>> GetByCategoryId(int id)
     {
-        return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id), Messages.ProductListed);
+        return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id),
+            Messages.ProductListed);
     }
 
     public IDataResult<List<Product>> GetByUnitPrice(decimal minPrice, decimal maxPrice)
     {
-        return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice > minPrice && p.UnitPrice < maxPrice));
+        return new SuccessDataResult<List<Product>>(_productDal.GetAll(p =>
+            p.UnitPrice > minPrice && p.UnitPrice < maxPrice));
     }
 
     public IDataResult<List<ProductDetailDto>> GetProductDetails()
@@ -53,5 +65,38 @@ public class ProductManager : IProductService
     public IDataResult<Product> GetById(int id)
     {
         return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == id));
+    }
+
+    private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+    {
+        var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+        if (result > 10)
+        {
+            return new ErrorResult("Category Hata");
+        }
+
+        return new SuccessResult();
+    }
+
+    private IResult CheckProductNameIsUnique(string productName)
+    {
+        var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+        if (result)
+        {
+            return new ErrorResult("Product Name Hata");
+        }
+
+        return new SuccessResult();
+    }
+
+    private IResult CheckIfCategoryLimitExceeded()
+    {
+        var result = _categoryService.GetAll();
+        if (result.Data.Count > 15)
+        {
+            return new ErrorResult();
+        }
+
+        return new SuccessResult();
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,21 +4,4 @@ using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 
 
-void NewFunction()
-{
-    var pm = new ProductManager(new EfProductDal());
-
-    var products = pm.GetByUnitPrice(10, 100);
-
-// foreach (var p in products) Console.WriteLine($"{p.ProductName}");
-
-    var om = new OrderManager(new EfOrderDal());
-    var orders = om.GetAll();
-    foreach (var o in orders)
-    {
-        Console.WriteLine($"{o.ShipperId}");
-    }
-}
-
-
-var pm = new ProductManager(new EfProductDal());
+Console.WriteLine();
